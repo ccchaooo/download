11c5164fac48a7875bba30c6d3108de07fd58458
dengchao <645111208@qq.com>
2019-11-28 18:32:25 +0800

    并发下载

---
+++ src/main/java/com/download/admin/utils/FileUtil.java
@@ -0,0 +1,40 @@
+package com.download.admin.utils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author dengchao
+ * @date 2019/11/28 0028 18:23
+ */
+public class FileUtil {
+
+    public static Map<String, String> files = new HashMap<>();
+
+    public Map<String, String> getFiles() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String path = "D:/DocDownload/";
+        File f = new File(path);
+        FileUtil.getFile(f);
+        System.out.println(files);
+    }
+
+
+    private static void getFile(File file) {
+        if (file != null) {
+            File[] f = file.listFiles();
+            if (f != null) {
+                for (int i = 0; i < f.length; i++) {
+                    getFile(f[i]);
+                }
+            } else {
+                System.out.println(file);
+                files.put(file.getName().substring(0, 5), file.getName());
+            }
+        }
+    }
+}
